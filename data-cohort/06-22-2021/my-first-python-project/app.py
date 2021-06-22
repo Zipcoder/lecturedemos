ad755b65b0f59bab9926ba0a914870ae02c004e1
git-leon <xleonhunter@gmail.com>
2021-06-22 11:59:27 -0400

    partitioned lecture demos

---
+++ data-cohort/06-22-2021/my-first-python-project/app.py
@@ -0,0 +1,30 @@
+'''
+hey check this out
+it's on multiple lines
+and
+it affects NOTHING
+'''
+five = 5
+ten = 10
+sum_of_five_and_ten = five + ten
+print(sum_of_five_and_ten)
+
+
+# Modulo operator and `type` function  example
+modulo_example = 4 % 2
+type_of_modulo_example = type(modulo_example)
+print(modulo_example)
+print(type_of_modulo_example)
+
+
+# hello world example
+hello_world = "\nHey world. It's me! Wheee! I'm programmin'"
+type_of_hello_world = type(hello_world) # fetches the type of the `hello_world` variable
+str_of_type_of_hello_world = str(type_of_hello_world)
+
+print(hello_world * 5)
+print(type_of_hello_world)
+print(hello_world + str_of_type_of_hello_world)
+
+
+sum_of_five_and_ten ** sum_of_five_and_ten
