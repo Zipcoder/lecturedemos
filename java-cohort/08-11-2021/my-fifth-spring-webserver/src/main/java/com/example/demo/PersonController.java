7c62f224dd713ef4faf38558b2a451bbd2dea94b
git-leon <xleonhunter@gmail.com>
2021-08-11 10:22:25 -0400

    brought 'em back to myspace daze

--- java-cohort/08-11-2021/my-fifth-spring-webserver/src/main/java/com/example/demo/PersonController.java
+++ java-cohort/08-11-2021/my-fifth-spring-webserver/src/main/java/com/example/demo/PersonController.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,28 +25,28 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @RequestMapping(method = RequestMethod.POST, value = "/create")
+    @PostMapping(value = "/create")
     // exposed at localhost:8080/person-controller/create
     public ResponseEntity<Person> create(@RequestBody Person person) { // `@RequestBody` allows us to inject JSON objects (representative of POJO instances) into our Spring server from our front-end client (mocked by Postman)
         return new ResponseEntity<>(service.create(person), HttpStatus.CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/read/{id}")
+    @GetMapping(value = "/read/{id}")
     public ResponseEntity<Person> read(@PathVariable Long id) {
         return new ResponseEntity<>(service.read(id), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/read")
+    @GetMapping(value = "/read")
     public ResponseEntity<List<Person>> readAll() {
         return new ResponseEntity<>(service.readAll(), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/update/{id}")
+    @PutMapping(value = "/update/{id}")
     public ResponseEntity<Person> update(@PathVariable Long id, @RequestBody Person newPersonData) {
         return new ResponseEntity<>(service.update(id, newPersonData), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
+    @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<Person> delete(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
