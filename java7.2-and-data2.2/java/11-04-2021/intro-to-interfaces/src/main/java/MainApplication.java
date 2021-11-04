c21b2af653e955dc88532ecd20285d0f004a85d5
git-leon <xleonhunter@gmail.com>
2021-11-04 13:38:09 -0400

    added boilerplate code to todays interface lecture

---
+++ java7.2-and-data2.2/java/11-04-2021/intro-to-interfaces/src/main/java/MainApplication.java
@@ -0,0 +1,10 @@
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 11/4/21 1:37 PM
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ java7.2-and-data2.2/java/11-04-2021/intro-to-interfaces/src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 11/4/21 1:37 PM
+ */
+public class SampleTest {
+    @Test
+    public void testSample() {
+        System.out.println("Hello world");
+    }
+}
