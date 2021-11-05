18c23362ef674a073266d57714893c8445ef80d3
git-leon <xleonhunter@gmail.com>
2021-11-05 09:58:50 -0400

    added test sample

---
+++ java7.2-and-data2.2/java/11-05-2021/untitled/src/main/java/MainApplication.java
@@ -0,0 +1,10 @@
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 11/5/21 9:58 AM
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ java7.2-and-data2.2/java/11-05-2021/untitled/src/test/java/TestSample.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 11/5/21 9:58 AM
+ */
+public class TestSample {
+    @Test
+    public void sampleTest() {
+        System.out.println("Sample");
+    }
+}
