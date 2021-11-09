1b80f89b4cf75e8cb3be709930728f53d1296cbe
git-leon <xleonhunter@gmail.com>
2021-11-09 10:37:19 -0500

    added boilerplate for this lecturedemo; lets hope I remember to push this up after we finish the lecture

---
+++ java7.2-and-data2.2/java/11-09-2021/inheritance/src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 11/9/21 10:36 AM
+ */
+public class SampleTest {
+    @Test
+    public void sample() {
+        System.out.println("Hello world");
+    }
+}
