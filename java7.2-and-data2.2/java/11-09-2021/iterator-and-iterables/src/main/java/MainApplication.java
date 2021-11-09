9950c5a838fa7d992d1e5e2bbd43978113d8a229
git-leon <xleonhunter@gmail.com>
2021-11-09 15:01:51 -0500

    added boilerplate code

---
+++ java7.2-and-data2.2/java/11-09-2021/iterator-and-iterables/src/main/java/MainApplication.java
@@ -0,0 +1,10 @@
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 11/9/21 3:01 PM
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ java7.2-and-data2.2/java/11-09-2021/iterator-and-iterables/src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 11/9/21 3:01 PM
+ */
+public class SampleTest {
+    @Test
+    public void test() {
+        System.out.println("Hey");
+    }
+}
