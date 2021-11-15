3e81411fd6092eb7c944a9e87a2675ead433fc11
git-leon <xleonhunter@gmail.com>
2021-11-15 18:17:29 -0500

    added SOLID design lecture

--- java7.2-and-data2.2/java/11-15-2021/solid-principles/src/test/java/InterfaceSegregationPrinciple.java
+++ java7.2-and-data2.2/java/11-15-2021/solid-principles/src/test/java/InterfaceSegregationPrinciple.java
@@ -1,6 +1,30 @@
-package PACKAGE_NAME;/**
+import org.junit.Test;
+
+/**
  * @author git-leon
- * @date 11/15/21 10:28 AM
  * @version 1.0.0
- */public class InterfaceSegregationPrinciple {
+ * @date 11/15/21 10:28 AM
+ */
+public class InterfaceSegregationPrinciple {
+    @Test
+    public void testPerson() {
+        Animal animal = new Animal();
+        Person person = new Person(0L, "Leon");
+        Serializable serializable = person;
+        Identifiable identifiable = person;
+        printAllIdentifiableObjects(identifiable, person, animal);
+        printAllSerializableObjects(serializable, person, animal);
+    }
+
+    private void printAllIdentifiableObjects(Identifiable... identifiables) {
+        for(Identifiable identifiable : identifiables) {
+            System.out.println(identifiable.identify());
+        }
+    }
+
+    private void printAllSerializableObjects(Serializable... serializables) {
+        for(Serializable serializable : serializables) {
+            System.out.println(serializable.serialize());
+        }
+    }
 }
--- java7.2-and-data2.2/java/11-15-2021/solid-principles/src/test/java/LiskovSubstitutionExample.java
+++ java7.2-and-data2.2/java/11-15-2021/solid-principles/src/test/java/LiskovSubstitutionExample.java
@@ -1,13 +1,34 @@
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author git-leon
  * @version 1.0.0
  * @date 11/15/21 9:58 AM
  */
-public class SampleTest {
+public class LiskovSubstitutionExample {
     @Test
     public void test() {
+        LinkedList<String> linkedList = new LinkedList<>();
+        ArrayList arrayList = new ArrayList<>(linkedList);
+        printAllElements(arrayList);
+        printAllElements(linkedList);
 
+    }
+
+    private void printAllElements(LinkedList<String> list) {
+        for(String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    private void printAllElements(ArrayList<String> list) {
+        for(String s : list) {
+            System.out.println(s);
+        }
     }
 }
