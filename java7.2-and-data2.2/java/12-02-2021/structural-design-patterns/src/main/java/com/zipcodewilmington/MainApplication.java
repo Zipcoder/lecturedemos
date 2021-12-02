0cb40c00d988acc950dd72209539149f67710ca9
git-leon <xleonhunter@gmail.com>
2021-12-02 16:11:51 -0500

    boilerplate content

---
+++ java7.2-and-data2.2/java/12-02-2021/structural-design-patterns/src/main/java/com/zipcodewilmington/MainApplication.java
@@ -0,0 +1,12 @@
+package com.zipcodewilmington;
+
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 12/2/21 4:10 PM
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ java7.2-and-data2.2/java/12-02-2021/structural-design-patterns/src/test/java/com/zipcodewilmington/SampleTest.java
@@ -0,0 +1,15 @@
+package com.zipcodewilmington;
+
+import org.junit.Test;
+
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 12/2/21 4:11 PM
+ */
+public class SampleTest {
+    @Test
+    public void test() {
+        System.out.println("Hello world");
+    }
+}
